3e447e1afccdf314e321d096b6617e2a6a563cad
dongho <dong0280@naver.com>
2023-02-12 22:55:24 +0900

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';//1/6 영상
+import 'package:test1/screens/current_location_screen.dart';
+import 'package:test1/screens/search_places_screen.dart';
+import 'package:test1/screens/simple_map_screen.dart';
+
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+//1/6 영상에 나옴
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Google Maps"),
+        centerTitle: true,
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                return const SimpleMapScreen();
+              }));
+            }, child: const Text("Simple Map")),
+
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                return const CurrentLocationScreen();
+              }));
+            }, child: const Text("User current location")),
+
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                return const SearchPlacesScreen();
+              }));
+            }, child: const Text("Search Places")),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
